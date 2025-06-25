c5f957c02ef02b60e0920fa6cbd9a1788ffc8be5
Sachin Rawat <sacrawat1@publicisgroupe.net>
2025-06-25 22:25:31 +0530

    checkov fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "bad_example" {
-  ami                    = "ami-0c55b159cbfafe1f0"  
-  instance_type          = "t2.large"              # Should fail by scp
+  ami                    = "ami-0c02fb55956c7d316"  # updated AMI
+  instance_type          = "t2.large"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.ssh_sg.id]
-  associate_public_ip_address = true                # Public exposure
+  associate_public_ip_address = true
 
   tags = {
     Name = "vulnerable-instance"
@@ -12,16 +12,10 @@ resource "aws_instance" "bad_example" {
 
 resource "aws_s3_bucket" "public_bucket" {
   bucket = "devsecops-public-test-demo"
-
-  acl    = "public-read"   # Checkov should catch this
   force_destroy = true
 
   tags = {
     Name        = "Public S3 Bucket"
     Environment = "dev"
   }
-}
-
-resource "random_id" "bucket_id" {
-  byte_length = 4
 }
